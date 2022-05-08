23485741301a0aae5a5bb1e3f282f8fa3ca78e81
jhonpaul123456 <70428997+jhonpaul123456@users.noreply.github.com>
2022-05-08 22:21:57 +0800

    Messaging

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { ServerErrorComponent } from './_errors/server-error/server-error.compon
 import { TestErrorsComponent } from './_errors/test-errors/test-errors.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,7 +21,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent},
-      { path: 'members/:username', component: MemberDetailComponent},
+      { path: 'members/:username', component: MemberDetailComponent, resolve: {member: MemberDetailedResolver}},
       { path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
       { path: 'lists', component: ListComponent }, 
       { path: 'messages', component: MessagesComponent },
--- client/src/app/components/member-detail/member-detail.component.ts
+++ client/src/app/components/member-detail/member-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 import { NgxGalleryImage,NgxGalleryOptions,NgxGalleryAnimation } from '@kolkov/ngx-gallery';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
+import { Message } from 'src/app/_models/message';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -10,13 +13,28 @@ import { NgxGalleryImage,NgxGalleryOptions,NgxGalleryAnimation } from '@kolkov/n
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs',{static: true}) memberTabs: TabsetComponent;
   member: Member;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
-  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
+  activeTab: TabDirective
+
+  messages: Message[] = [];
+  constructor(private memberService: MembersService, private route: ActivatedRoute, 
+    private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.loadMember();
+ 
+
+    this.route.data.subscribe(data => {
+      this.member = data.member;
+    })
+
+    this.route.queryParams.subscribe(params => {
+      params.tab ? this.selectTab(params.tab) : this.selectTab(0);
+    })
+
+
     this.galleryOptions = [
       {
         width: '500px',
@@ -28,6 +46,7 @@ export class MemberDetailComponent implements OnInit {
       }
     ]
 
+    this.galleryImages = this.getImages();
 
   }
 
@@ -43,11 +62,22 @@ export class MemberDetailComponent implements OnInit {
     return imageUrls;
   }
 
-  //kuha ug detail gkan member
-  loadMember() {
-    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
-      this.member = member;
-      this.galleryImages = this.getImages();
+
+
+  loadMessages() {
+    this.messageService.getMessageThread(this.member.username).subscribe(messages => {
+      this.messages = messages;
     })
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
+  onTabActivated(data: TabDirective){
+    this.activeTab = data;
+    if(this.activeTab.heading === 'Messages' && this.messages.length === 0){
+      this.loadMessages();
+
+    }
   }
 }
